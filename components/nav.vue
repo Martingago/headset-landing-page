5002710468043ad0981dfbdeedfedb638f75c3c7
Martin <gagochorenmartin@gmail.com>
2022-08-31 16:09:07 +0200

    menu principal

--- components/nav.vue
+++ components/nav.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-navigation">
         <nav class="navigation-page">
-            <button class="btn-menu">
+            <button class="btn-menu" @click="mostrar = !mostrar">
                         <font-awesome-icon icon="fa-solid fa-bars" />
             </button>
             <ul class="logo-button">
@@ -9,20 +9,22 @@
                     <NuxtLink to="/"><h2>audiophile</h2></NuxtLink>
                 </li>
             </ul>
-            <ul class="nav-buttons">
-                <li>
-                    <NuxtLink to="/about">Inicio</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/">Auriculares</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/">Altavoces</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/">In-Ear</NuxtLink>
-                </li>
-            </ul>
+            <Transition>
+                <ul class="nav-buttons" v-if="mostrar">
+                    <li>
+                        <NuxtLink to="/about">Inicio</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/">Auriculares</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/">Altavoces</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/">In-Ear</NuxtLink>
+                    </li>
+                </ul>
+            </Transition>
             <ul class="chart-button">
                 <li>
                     <NuxtLink to="/"> <font-awesome-icon icon="fa-solid fa-cart-shopping" /></NuxtLink>
@@ -32,5 +34,8 @@
     </div>
 </template>
 <script setup>
-import "@/assets/css/components/nav.css"
+import "@/assets/css/components/nav.css";
+import { ref } from "vue";
+
+const mostrar = ref(false)
 </script>
